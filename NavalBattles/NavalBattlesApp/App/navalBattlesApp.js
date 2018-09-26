4378775ba6f30dac960f61f81d417e1b77f1120c
Francesco Cossu <francesco.cossu@gmail.com>
2018-09-26 21:23:20 +0100

    reset

--- NavalBattles/NavalBattlesApp/App/navalBattlesApp.js
+++
@@ -1,8 +0,0 @@
-﻿angular.module("navalBattlesApp", [
-    "navalBattlesApp.controllers",
-    "navalBattlesApp.services",
-    "navalBattlesApp.directives"]);
-
-angular.module("navalBattlesApp.controllers", []);
-angular.module("navalBattlesApp.services", []);
-angular.module("navalBattlesApp.directives", []);
